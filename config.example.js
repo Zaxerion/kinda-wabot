6032fe422f1c56ab8b93b0f0a50836b37bf2f6f1
Zaxerion <ferdisaputra488@gmail.com>
2025-10-28 22:43:35 +0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -23,6 +23,12 @@ global.eror = '```404 error```'
 
 global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
 
+
+const res = await fetch(global.API("zax", "/api/etc/thumbnail", { type: 'large' }, "apikey"), { responseType: 'arraybuffer' });
+const arrayBuffer = await res.arrayBuffer();
+global.thumbnail = Buffer.from(arrayBuffer);
+global.imageId = res.headers.get("x-image-id");
+
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
 	unwatchFile(file)
